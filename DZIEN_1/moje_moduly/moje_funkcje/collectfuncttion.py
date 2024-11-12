2e7a4cec44e55b3b2b48f39e1a1674469a1578aa
albim72 <marcin.albiniak72@gmail.com>
2024-11-12 10:10:05 +0100

    Create collectfuncttion.py

---
+++ DZIEN_1/moje_moduly/moje_funkcje/collectfuncttion.py
@@ -0,0 +1,7 @@
+def czytaj_liste(lista):
+    for i,j in enumerate(lista):
+        print(f"numer indeksu elementu listy: {i}, wartośc elementu: {j}")
+        
+def czytaj_slownik(slownik):
+    for x,y in slownik.items():
+        print(f"ELEMENT -> {x}: {y}")
